acca5b736ed39e42a67e953578644a703ce31826
NC-007 <86400910+NC-007@users.noreply.github.com>
2021-07-01 13:26:52 +0200

    Tests for border control rules

--- FHICORC.Tests/TestMocks/MockBusinessRulesService.cs
+++ FHICORC.Tests/TestMocks/MockBusinessRulesService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using FHICORC.Core.Services.Interface;
 using FHICORC.Core.Services.Model.BusinessRules;
+using FHICORC.Services.Mocks;
 
 namespace FHICORC.Tests.TestMocks
 {
@@ -24,41 +25,10 @@ namespace FHICORC.Tests.TestMocks
 
         public ICollection<BusinessRule> ReadBusinessRules()
         {
-            return new List<BusinessRule>
-            {
-                new BusinessRule
-                {
-                    Identifier = "GR-NO-0001"
-                },
-                new BusinessRule
-                {
-                    Identifier = "GR-NO-0002"
-                },
-                new BusinessRule
-                {
-                    Identifier = "VR-NO-0001"
-                },
-                new BusinessRule
-                {
-                    Identifier = "TR-NO-0001"
-                },
-                new BusinessRule
-                {
-                    Identifier = "TR-NO-0002"
-                },
-                new BusinessRule
-                {
-                    Identifier = "RR-NO-0001"
-                },
-                new BusinessRule
-                {
-                    Identifier = "RR-NO-0002"
-                },
-                new BusinessRule
-                {
-                    Identifier = "RR-NO-0003"
-                },
-            };
+            var repo = new MockBusinessRulesRepository();
+            var result = repo.GetBusinessRules().GetAwaiter().GetResult();
+
+            return result.Data;
         }
     }
 }
