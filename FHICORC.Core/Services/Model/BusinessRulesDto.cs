8093e58a47b8ea7a0a9ee9adc128f99738db016c
NC-001 <86482015+NC-001@users.noreply.github.com>
2021-07-02 09:38:04 +0200

    Added business rules dto

---
+++ FHICORC.Core/Services/Model/BusinessRulesDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace FHICORC.Core.Services.Model
+{
+    public class BusinessRulesDto
+    {
+        public string International { get; set; }
+        public string National { get; set; }
+    }
+}
