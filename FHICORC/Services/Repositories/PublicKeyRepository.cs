8d35d382bffddc6addd493711635b122de9f1a0c
NC-007 <86400910+NC-007@users.noreply.github.com>
2021-08-04 15:34:46 +0200

    Enabled 500 error to test generic error screen

--- FHICORC/Services/Repositories/PublicKeyRepository.cs
+++ FHICORC/Services/Repositories/PublicKeyRepository.cs
@@ -13,7 +13,10 @@ namespace FHICORC.Services.WebServices
         public async Task<ApiResponse<List<PublicKeyDto>>> GetPublicKey()
         {
             string url = Urls.URL_GET_PUBLIC_KEY;
-            return await _restClient.Get<List<PublicKeyDto>>(url);
+            var response = await _restClient.Get<List<PublicKeyDto>>(url);
+            response.StatusCode = 500;
+            response.ErrorType = ServiceErrorType.ServerError;
+            return response;
         }
     }
 }
