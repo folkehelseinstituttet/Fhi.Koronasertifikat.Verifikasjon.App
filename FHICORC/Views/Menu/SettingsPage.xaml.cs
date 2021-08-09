ba79c87e150d10577416add3859180103fbf05b9
Antoni Kistowski <87481390+NC-004@users.noreply.github.com>
2021-08-09 17:33:28 +0200

    defect 237 fix, navigation header icon accessibility size fix

--- FHICORC/Views/Menu/SettingsPage.xaml.cs
+++ FHICORC/Views/Menu/SettingsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using FHICORC.Configuration;
+using FHICORC.Controls;
 using FHICORC.Services.Interfaces;
 using FHICORC.Utils;
 using FHICORC.ViewModels.Menu;
@@ -13,16 +14,29 @@ namespace FHICORC.Views.Menu
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
+        private Label _labelSettings;
+        private Switch _switchToggledSound;
+        private Switch _switchToggledVibration;
+        private SingleLineButton _fetchSingleLineButton;
+        private double _fontSize;
+
         public SettingsPage(SettingsPageViewModel viewModel)
         {
             InitializeComponent();
             BindingContext = viewModel;
+            _labelSettings = this.FindByName<Label>("LabelSettings");
+            _switchToggledSound = this.FindByName<Switch>("SwitchToggledSound");
+            _switchToggledVibration = this.FindByName<Switch>("SwitchToggledVibration");
+            _fetchSingleLineButton = this.FindByName<SingleLineButton>("FetchSingleLineButton");
+            _fontSize = _labelSettings.FontSize;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
             IoCContainer.Resolve<INavigationService>().SetStatusBar(FHICORCColor.NavigationHeaderBackgroundColor.Color(), Color.Black);
+            UpdateIconsSizesAccessibility();
+
         }
 
         void Switch_Toggled_Sound(object sender, ToggledEventArgs e)
@@ -57,6 +71,40 @@ namespace FHICORC.Views.Menu
             {
                 RadioButtonEnglish.IsChecked = true;
             }
+        }
+        private void UpdateIconsSizesAccessibility()
+        {
+            int threshold = 22;
+            double delta = _fontSize - threshold;
+            double scaleValue = 1.0;
+
+            if (Device.RuntimePlatform == Device.iOS && _fontSize > threshold)
+            {
+                scaleValue = 1.0 + 0.01 * delta;
+
+            }
+            else if (Device.RuntimePlatform == Device.Android && _fontSize > threshold)
+            {
+                scaleValue = 1.0 + 0.015 * delta;
+            }
+            
+            if (scaleValue >= 1.4)
+            {
+                scaleValue = 1.4;
+            }
+            else if (scaleValue < 1.0)
+            {
+                scaleValue = 1.0;
+            }
+            double scaleValueButton = scaleValue;
+            if (scaleValueButton >= 1.1)
+            {
+                scaleValueButton = 1.1;
+            }
+            _switchToggledSound.Scale = scaleValue;
+            _switchToggledVibration.Scale = scaleValue;
+            _fetchSingleLineButton.Scale = scaleValueButton;
+
         }
     }
 }
