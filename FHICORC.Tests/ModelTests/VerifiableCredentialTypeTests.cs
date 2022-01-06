eb1c4191cdce25c12d0e73c4244de704350a444b
NC-204 <97104440+NC-204@users.noreply.github.com>
2022-01-06 12:38:58 +0100

    Added verification of smart health card(SHC) verifiable credential(VC) type. (#103)
    * Check that type contains covid19 and health card
    * Move decoding of SHC QR into its own method, to separate logic.

---
+++ FHICORC.Tests/ModelTests/VerifiableCredentialTypeTests.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System;
+using NUnit.Framework;
+using FHICORC.Core.Services.Model;
+
+namespace FHICORC.Tests.Models
+{
+    public class VerifiableCredentialTypeTests
+    {
+        public VerifiableCredentialTypeTests()
+        {
+        }
+
+        [Test]
+        public void VerifyType_MissingJsonKey_ThrowsException()
+        {
+            string json = "{\"iss\":\"iss\",\"nbf\":110,\"vc\":{}}";
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                VerifiableCredentialTypeSupport.VerifyType(json);
+            });
+        }
+
+        [Test]
+        public void VerifyType_NoTypes_ThrowsException()
+        {
+            string json = "{\"iss\":\"iss\",\"nbf\":110,\"vc\":{\"type\":[]}}";
+
+            Assert.Throws<InvalidDataException>(() =>
+            {
+                VerifiableCredentialTypeSupport.VerifyType(json);
+            });
+        }
+
+        [Test]
+        public void VerifyType_OnlyCovid19Type_ThrowsException()
+        {
+            string json = "{\"iss\":\"iss\",\"nbf\":110,\"vc\":{\"type\":[" +
+                "\"https://smarthealth.cards#covid19\"]}}";
+
+            Assert.Throws<InvalidDataException>(() =>
+            {
+                VerifiableCredentialTypeSupport.VerifyType(json);
+            });
+        }
+
+        [Test]
+        public void VerifyType_OnlyHealthCardType_ThrowsException()
+        {
+            string json = "{\"iss\":\"iss\",\"nbf\":110,\"vc\":{\"type\":[" +
+                "\"https://smarthealth.cards#health-card\"]}}";
+
+            Assert.Throws<InvalidDataException>(() =>
+            {
+                VerifiableCredentialTypeSupport.VerifyType(json);
+            });
+        }
+
+        [Test]
+        public void VerifyType_Covid19AndHealthCardType_DoesNotThrowException()
+        {
+            string json = "{\"iss\":\"iss\",\"nbf\":110,\"vc\":{\"type\":[" +
+                "\"https://smarthealth.cards#health-card\"," +
+                "\"https://smarthealth.cards#covid19\"]}}";
+
+            Assert.DoesNotThrow(() =>
+            {
+                VerifiableCredentialTypeSupport.VerifyType(json);
+            });
+        }
+
+
+        [Test]
+        public void VerifyType_Covid19AndHealthCardType_AndUnkownType_DoesNotThrowException()
+        {
+            string json = "{\"iss\":\"iss\",\"nbf\":110,\"vc\":{\"type\":[" +
+                "\"https://smarthealth.cards#health-card\"," +
+                   "\"someUnknownType\"," +
+                "\"https://smarthealth.cards#covid19\"]}}";
+
+            Assert.DoesNotThrow(() =>
+            {
+                VerifiableCredentialTypeSupport.VerifyType(json);
+            });
+        }
+    }
+}
