d55b2ce643a54b1dfaffad361335b8cb851a8234
NC-001 <86482015+NC-001@users.noreply.github.com>
2022-01-11 15:05:54 +0100

    removed constructor from verifiableCredential

--- FHICORC.Core/Services/Model/SmartHealthCardModel/Shc/VerifiableCredential.cs
+++ FHICORC.Core/Services/Model/SmartHealthCardModel/Shc/VerifiableCredential.cs
@@ -6,12 +6,6 @@ namespace FHICORC.Core.Services.Model.SmartHealthCardModel.Shc
 {
     public class VerifiableCredential
     {
-        public VerifiableCredential(List<VerifiableCredentialType> VerifiableCredentialTypeList, CredentialSubject CredentialSubject)
-        {
-            this.VerifiableCredentialTypeList = VerifiableCredentialTypeList;
-            this.CredentialSubject = CredentialSubject;
-        }
-
         [JsonProperty(propertyName: "type", ItemConverterType = typeof(VerifiableCredentialTypeConverter), Required = Required.Always)]
         public List<VerifiableCredentialType> VerifiableCredentialTypeList { get; set; }
 
