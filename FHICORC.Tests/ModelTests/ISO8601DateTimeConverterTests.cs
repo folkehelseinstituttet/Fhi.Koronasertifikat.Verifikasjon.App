f2035de7fc6cc0e965bdb6d9fce353c9fe62e577
NC-001 <86482015+NC-001@users.noreply.github.com>
2022-01-11 15:34:06 +0100

    updated expected date to correct time zone

--- FHICORC.Tests/ModelTests/ISO8601DateTimeConverterTests.cs
+++ FHICORC.Tests/ModelTests/ISO8601DateTimeConverterTests.cs
@@ -19,7 +19,7 @@ namespace FHICORC.Tests.ModelTests
         [TestCase("1951-03-01 00:00:00", "{\"datetime\":\"1951-03\"}")]
         [TestCase("1951-01-01 00:00:00", "{\"datetime\":\"1951\"}")]
         [TestCase("2021-02-28 05:50:06", "{\"datetime\":\"2021-02-28 05:50:06\"}")]
-        [TestCase("2015-02-07 19:28:17", "{\"datetime\":\"2015-02-07T13:28:17-05:00\"}")]
+        [TestCase("2015-02-07 18:28:17", "{\"datetime\":\"2015-02-07T13:28:17-05:00\"}")]
         [TestCase("2017-01-01 00:00:00", "{\"datetime\":\"2017-01-01T00:00:00.000Z\"}")]
         public void ISO8601DateTimeConverter_ShouldParseJsonToDateTime(DateTime expectedDate, string dateTime)
         {
