f5ca68392a677a511b3c75aaa1fd4f869581e8e9
NC-001 <86482015+NC-001@users.noreply.github.com>
2022-01-17 11:21:52 +0100

    Feature/shc scanner result (#108)
    * Created screen for shc scanner result
    
    * Updated UI for SHC scanner result
    
    * Added showing info from vaccine info in SHC result.
    
    * removed netstandard2.0
    
    Co-authored-by: NC-204 <NC-204@protonmail.com>

---
+++ FHICORC.Core/Services/Model/SmartHealthCardModel/Shc/SmartHealthCardWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using FHICORC.Core.Services.Interface;
+using FHICORC.Core.Services.Model.SmartHealthCardModel.Coding;
+
+namespace FHICORC.Core.Services.Model.SmartHealthCardModel.Shc
+{
+    public class SmartHealthCardWrapper : ITokenPayload
+    {
+        public SmartHealthCardWrapper(SmartHealthCardModel smartHealthCardModel,
+            List<SmartHealthCardVaccineInfo> vaccineInfo)
+        {
+            SmartHealthCard = smartHealthCardModel;
+            VaccineInfo = vaccineInfo;
+        }
+
+        public SmartHealthCardModel SmartHealthCard { get; private set; }
+
+        public List<SmartHealthCardVaccineInfo> VaccineInfo { get; private set; }
+
+        public DateTime? ExpiredDateTime() => SmartHealthCard.ExpirationDate;
+
+        public DateTime? IssueDateTime() => SmartHealthCard.IssuanceDate;
+    }
+}
