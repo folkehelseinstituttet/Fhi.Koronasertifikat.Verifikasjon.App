192343753d50b76587ce2b9e5e6ccdc992e35e04
NC-204 <97104440+NC-204@users.noreply.github.com>
2022-02-03 08:26:49 +0100

    Add validation on shc wrapper for number of patients and expiry date. (#113)
    * Adjust expiry screen with different text for border control expiry. Also split page title and title into different variables, so all is set in code (not one hardcoded in xaml).

--- FHICORC.Tests/Factories/SmartHealthCardFactory.cs
+++ FHICORC.Tests/Factories/SmartHealthCardFactory.cs
@@ -1,4 +1,7 @@
-﻿using FHICORC.Core.Services.Model.SmartHealthCardModel.Shc;
+﻿using System.Collections.Generic;
+using FHICORC.Core.Services.Model.SmartHealthCardModel.Coding;
+using FHICORC.Core.Services.Model.SmartHealthCardModel.Shc;
+using Newtonsoft.Json.Linq;
 
 namespace FHICORC.Tests.Factories
 {
@@ -42,6 +45,11 @@ namespace FHICORC.Tests.Factories
                 "\"y\":\"" + y + "\"}]}";
         }
 
+        public static string CreateFhirBundleJson()
+        {
+            return "{\"resourceType\":\"Bundle\",\"type\":\"collection\",\"entry\":[{\"fullUrl\":\"resource:0\",\"resource\":{\"resourceType\":\"Patient\",\"name\":[{\"family\":\"Anyperson\",\"given\":[\"John\",\"B.\"]}],\"birthDate\":\"1951-01-20\"}},{\"fullUrl\":\"resource:1\",\"resource\":{\"resourceType\":\"Immunization\",\"meta\":{\"security\":[{\"system\":\"https://smarthealth.cards/ial\",\"code\":\"IAL1.2\"}]},\"status\":\"completed\",\"vaccineCode\":{\"coding\":[{\"system\":\"http://hl7.org/fhir/sid/cvx\",\"code\":\"207\"}]},\"patient\":{\"reference\":\"resource:0\"},\"occurrenceDateTime\":\"2021-01-01\",\"performer\":[{\"actor\":{\"display\":\"ABC General Hospital\"}}],\"lotNumber\":\"0000001\"}},{\"fullUrl\":\"resource:2\",\"resource\":{\"resourceType\":\"Immunization\",\"meta\":{\"security\":[{\"system\":\"https://smarthealth.cards/ial\",\"code\":\"IAL1.2\"}]},\"status\":\"completed\",\"vaccineCode\":{\"coding\":[{\"system\":\"http://hl7.org/fhir/sid/cvx\",\"code\":\"207\"}]},\"patient\":{\"reference\":\"resource:0\"},\"occurrenceDateTime\":\"2021-01-29\",\"performer\":[{\"actor\":{\"display\":\"ABC General Hospital\"}}],\"lotNumber\":\"0000007\"}}]}";
+        }
+
         public static SmartHealthCardImmunization CreateImmunization()
         {
             SmartHealthCardImmunization immunization = new SmartHealthCardImmunization();
@@ -61,6 +69,27 @@ namespace FHICORC.Tests.Factories
             seccondCoding.Code = "code2";
             codeableConcept.Coding = new SmartHealthCardCoding[] { firstCoding, seccondCoding };
             return codeableConcept;
+        }
+
+        public static CredentialSubject CreateCredentialSubject()
+        {
+            JObject fhirBundle = JObject.Parse(CreateFhirBundleJson());
+            return new CredentialSubject("4.1", fhirBundle);
+        }
+
+        public static SmartHealthCardWrapper CreateWrapper()
+        {
+            SmartHealthCardWrapper wrapper = new SmartHealthCardWrapper(
+                new SmartHealthCardModel()
+                {
+                    VerifiableCredential = new VerifiableCredential()
+                    {
+                        CredentialSubject = CreateCredentialSubject()
+                    }
+                },
+                new List<SmartHealthCardVaccineInfo>()
+                );
+            return wrapper;
         }
     }
 }
---
+++ FHICORC.Tests/ModelTests/SmartHealthCardWrapperTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using FHICORC.Core.Services.Enum;
+using FHICORC.Core.Services.Model.SmartHealthCardModel.Shc;
+using FHICORC.Tests.Factories;
+using NUnit.Framework;
+
+namespace FHICORC.Tests.ModelTests
+{
+    public class SmartHealthCardWrapperTests
+    {
+        private SmartHealthCardWrapper wrapper;
+
+        [SetUp]
+        public void Init()
+        {
+            wrapper = SmartHealthCardFactory.CreateWrapper();
+        }
+
+        [Test]
+        public void Validate_Valid()
+        {
+            TokenValidateResult result = wrapper.Validate();
+
+            Assert.AreEqual(TokenValidateResult.Valid, result);
+        }
+
+        [Test]
+        public void Validate_WithExpiredDate_IsExpired()
+        {
+            wrapper.SmartHealthCard.ExpirationDate = new DateTime(2019, 1, 30, 0, 0, 0, DateTimeKind.Utc);
+
+            TokenValidateResult result = wrapper.Validate();
+
+            Assert.AreEqual(TokenValidateResult.Expired, result);
+        }
+
+        [Test]
+        public void Validate_WithMultiplePatiants_IsInvalid()
+        {
+            wrapper.SmartHealthCard.VerifiableCredential.CredentialSubject.Patients.Add(
+                new SmartHealthCardPatient()
+                );
+
+            TokenValidateResult result = wrapper.Validate();
+
+            Assert.AreEqual(TokenValidateResult.Invalid, result);
+        }
+    }
+}
